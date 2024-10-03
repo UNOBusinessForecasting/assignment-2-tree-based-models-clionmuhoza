1f74bb1db20e3d230dd672871b7e04b2703dd1c9
clionmuhoza <148167981+clionmuhoza@users.noreply.github.com>
2024-10-03 18:18:46 -0500

    Add files via upload

--- assignment2.py
+++ assignment2.py
@@ -13,26 +13,31 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+# %%
 TrainData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv')
 
+# %%
 Y = TrainData['meal']
 X = TrainData.drop(columns=['meal', 'id', 'DateTime'], axis=1)
-
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
 
+# %%
 model = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=42, class_weight="balanced")
 modelFit = model.fit(x_train, y_train)
 
+# %%
 y_pred = model.predict(x_test)
 acc = accuracy_score(y_test, y_pred)
 print(f"Model accuracy is {acc * 100:.2f}%.")
 
+# %%
 TestData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv')
 
+# %%
 xt = TestData.drop(columns=['meal', 'id', 'DateTime'], axis=1)
 
+# %%
 pred = model.predict(xt)
-
 pred = [int(p) for p in pred]
 
 print(pred)
