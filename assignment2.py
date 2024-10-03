614fac97498709e9d8dd166fcf09b7056fdc32b5
clionmuhoza <148167981+clionmuhoza@users.noreply.github.com>
2024-10-03 18:53:06 -0500

    Add files via upload

--- assignment2.py
+++ assignment2.py
@@ -7,29 +7,61 @@ Original file is located at
     https://colab.research.google.com/drive/1dB7dx10vwkcRRGCVJYLG2JyiqrJ_WS-O
 """
 
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
-data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
+TrainData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv')
 
-model = DecisionTreeClassifier(max_depth=10, min_samples_leaf=10)
+Y = TrainData['meal']
+X = TrainData.drop(columns=['meal', 'id', 'DateTime'], axis=1)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=40)
 
-Y = data["meal"]
-X = data.drop(["meal", "id", "DateTime"], axis=1)
+model = RandomForestClassifier(n_estimators=100, random_state=40)
+modelFit = model.fit(x_train, y_train)
 
-x, xt, y, yt = train_test_split(X, Y, test_size=0.33, random_state=42)
+feature_scores = pd.Series(model.feature_importances_, index=x_train.columns).sort_values(ascending=False)
+print(feature_scores)
 
-modelFit = model.fit(x, y)
+predict = model.predict(x_test)
+acc = accuracy_score(y_test, predict)
+print("Model accuracy is {}%.".format(acc * 100))
 
-print("\n\nIn-sample accuracy: %s%%\n\n" % str(round(100 * accuracy_score(y, model.predict(x)), 2)))
-print("\n\nOut-of-sample accuracy: %s%%\n\n" % str(round(100 * accuracy_score(yt, model.predict(xt)), 2)))
+train_accuracy = accuracy_score(y_train, model.predict(x_train))
+test_accuracy = accuracy_score(y_test, predict)
+print(f"\nTraining accuracy: {train_accuracy * 100:.2f}%")
+print(f"Test accuracy: {test_accuracy * 100:.2f}%")
 
-test = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
-testNew = test.drop(["meal", "id", "DateTime"], axis=1)
-
-pred = modelFit.predict(testNew)
-pred = [int(p) for p in pred]
-
+TestData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv')
+xt = TestData.drop(columns=['meal', 'id', 'DateTime'], axis=1)
+pred = model.predict(xt)
 print(pred)
+
+predTest2 = model.predict(xt)
+pred2 = pd.DataFrame(predTest2, columns=["predict_meal"])
+pred2["predict_meal"] = pred2["predict_meal"].astype(int)
+print("\nRandom Forest Predictions on test data:")
+print(pred2.value_counts())
+
+predTest3 = DecisionTreeClassifier(max_depth=10, min_samples_leaf=10).fit(x_train, y_train).predict(xt)
+pred3 = pd.DataFrame(predTest3, columns=["predict_meal"])
+pred3["predict_meal"] = pred3["predict_meal"].astype(int)
+print("\nDecision Tree Predictions on test data:")
+print(pred3.value_counts())
+
+pred_train = model.predict(x_train)
+cm = confusion_matrix(y_train, pred_train)
+print('Confusion matrix\n\n', cm)
+
+def test_model_accuracy(model, x_test, y_test):
+    pred = model.predict(x_test)
+    return accuracy_score(y_test, pred)
+
+rf_test_acc = test_model_accuracy(model, x_test, y_test)
+dt_test_acc = test_model_accuracy(DecisionTreeClassifier(max_depth=10, min_samples_leaf=10).fit(x_train, y_train), x_test, y_test)
+
+print(f"\nRandom Forest Test accuracy: {rf_test_acc * 100:.2f}%")
+print(f"Decision Tree Test accuracy: {dt_test_acc * 100:.2f}%")
