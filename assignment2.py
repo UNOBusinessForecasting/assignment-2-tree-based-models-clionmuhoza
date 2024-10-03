ea5c8a558caaa99c62492d7a67434da06c9d51d1
clionmuhoza <148167981+clionmuhoza@users.noreply.github.com>
2024-10-03 18:14:53 -0500

    Add files via upload

--- assignment2.py
+++ assignment2.py
@@ -9,35 +9,30 @@ Original file is located at
 
 # %%
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# %%
 TrainData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv')
-TrainData.head()
-
-# %%
-from sklearn.model_selection import train_test_split
 
 Y = TrainData['meal']
-X = TrainData.drop(columns=['meal','id','DateTime'], axis=1)
+X = TrainData.drop(columns=['meal', 'id', 'DateTime'], axis=1)
+
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
 
-# %%
+model = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=42, class_weight="balanced")
+modelFit = model.fit(x_train, y_train)
 
-model = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state= 42, class_weight = "balanced")
-modelFit = model.fit(x_train,y_train)
+y_pred = model.predict(x_test)
+acc = accuracy_score(y_test, y_pred)
+print(f"Model accuracy is {acc * 100:.2f}%.")
 
-# Test our model using the testing data
-predict = model.predict(x_test)
-acc = accuracy_score(y_test, predict)
-
-print("Model accuracy is {}%.".format(acc*100))
-
-# %%
 TestData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv')
-xt = TestData.drop(columns = ['meal','id','DateTime'], axis =1)
+
+xt = TestData.drop(columns=['meal', 'id', 'DateTime'], axis=1)
+
 pred = model.predict(xt)
-pred
+
+pred = [int(p) for p in pred]
+
+print(pred)
